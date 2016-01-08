24e525f5454c2989eb194c43b4abf44bbfcc77f0
Christopher Lee <yensen@gmail.com>
2016-01-08 09:40:49 -0800

    successfully read bitmap file into buffer

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-test
+const fs = require('fs');
+var bitmap = fs.readFileSync(process.argv[2]);
+
+console.log(`type: ${bitmap.toString('utf8', 0, 2)}`);
+console.log(`size: ${bitmap.readUInt32LE(2)}`);
+console.log(`start of pixel data: ${bitmap.readUInt32LE(10)}`);
+console.log(`width: ${bitmap.readUInt32LE(18)}`);
+console.log(`height: ${bitmap.readUInt32LE(22)}`);
+console.log(`number of colors: ${bitmap.readUInt32LE(46)}`)
+console.log(bitmap);

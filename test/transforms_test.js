71a28c01a2f401e011b2e543519fd233cdac42b2
Christopher Lee <yensen@gmail.com>
2016-01-11 11:27:08 -0800

    added grayscale transform, command line interface can select specific transform type

--- test/transforms_test.js
+++ test/transforms_test.js
@@ -1,9 +1,9 @@
-var invert = require('../lib/invert');
+var transforms = require('../lib/transforms');
 var fs = require('fs');
 var expect = require('chai').expect;
 var bufferObject;
 
-describe('the invert function', function() {
+describe('the transforms object', function() {
   before(function(done) {
     fs.readFile('./img/palette-bitmap.bmp', function(err, data) {
       bufferObject = data;
@@ -11,12 +11,18 @@ describe('the invert function', function() {
     });
   });
 
-  it('miranda does not care', function() {
-    expect(typeof invert).to.eql('function');
-  });
-
   it('should contain a buffer', function() {
     expect(Buffer.isBuffer(bufferObject)).to.eql(true);
   });
+  
+  it('contains a invert method', function() {
+    expect(typeof transforms.invert).to.eql('function');
+  });
+
+  it('contains a grayscale method', function() {
+    expect(typeof transforms.grayscale).to.eql('function');
+  });
+
+
 
 });
